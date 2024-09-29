5fea2e25073094553242153cbb1e260c4078695d
BohdanKulish <bogdankulish5@gmail.com>
2024-09-29 13:16:41 +0300

    Added sources domain entities

--- src/Modules/Sources/AlternativeEnergy.Sources.Domain/Repositories/IUserEnergyChoiceRepository.cs
+++ src/Modules/Sources/AlternativeEnergy.Sources.Domain/Repositories/IUserEnergyChoiceRepository.cs
@@ -3,7 +3,7 @@ using AlternativeEnergy.Sources.Domain.Entities;
 
 namespace AlternativeEnergy.Sources.Domain.Repositories
 {
-    public interface ISourceRepository : IRepositoryBase<Source, Guid>
+    public interface IUserEnergyChoiceRepository : IRepositoryBase<UserEnergyChoice, Guid>
     {
     }
 }
