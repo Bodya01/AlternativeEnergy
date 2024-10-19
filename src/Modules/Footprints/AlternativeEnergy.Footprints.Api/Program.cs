40f4ce837aa60f2a385a63e47178e044d1b8cd4a
BohdanKulish <bogdankulish5@gmail.com>
2024-10-20 02:01:29 +0300

    Fixed build issue

---
+++ src/Modules/Footprints/AlternativeEnergy.Footprints.Api/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AlternativeEnergy.Footprints.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { });
+    }
+}
